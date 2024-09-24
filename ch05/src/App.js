2c068f893ada2b56dd7fd2c03a354d83ecfa2420
hado0123 <hado_123@naver.com>
2024-09-24 23:19:34 +0900

    up

--- ch05/src/App.js
+++ ch05/src/App.js
@@ -4,6 +4,8 @@ import IterationSample3 from './IterationSample3'
 import IterationSample4 from './IterationSample4'
 import IterationSample5 from './IterationSample5'
 import IterationSample6 from './IterationSample6'
+import ConcatEx from './ConcatEx'
+import PushEx from './PushEx'
 
 function App() {
    //return <IterationSample />
@@ -11,7 +13,9 @@ function App() {
    //return <IterationSample3 />
    //return <IterationSample4 />
    //return <IterationSample5 />
-   return <IterationSample6 />
+   // return <IterationSample6 />
+   return <ConcatEx />
+   // return <PushEx />
 }
 
 export default App
