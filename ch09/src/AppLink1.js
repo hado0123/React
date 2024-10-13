8fe610f53a6fd1098c1b7440b03717dcb77f9062
hado0123 <hado_123@naver.com>
2024-10-13 15:49:17 +0900

    up

---
+++ ch09/src/AppLink1.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg'
+import './App.css'
+import { Route, Routes, Link } from 'react-router-dom'
+
+function Home() {
+   return (
+      <div>
+         <h2>Home</h2>
+         Home...
+      </div>
+   )
+}
+
+function Topics() {
+   return (
+      <div>
+         <h2>Topics</h2>
+         Topics...
+      </div>
+   )
+}
+
+function Contact() {
+   return (
+      <div>
+         <h2>Contact</h2>
+         Contact...
+      </div>
+   )
+}
+
+function App() {
+   return (
+      <div>
+         <h1>리액트 라우터</h1>
+         <ul>
+            <li>
+               <Link to="/">Home</Link>
+            </li>
+            <li>
+               <Link to="/topics">Topics</Link>
+            </li>
+            <li>
+               <Link to="/contact">Contact</Link>
+            </li>
+         </ul>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/topics" element={<Topics />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/*" element={'Not Found'} />
+         </Routes>
+      </div>
+   )
+}
+
+export default App
