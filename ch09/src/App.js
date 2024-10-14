4fca7745bea5cc4d469a1bfbb1f10568cb376943
hado0123 <hado_123@naver.com>
2024-10-14 16:28:35 +0900

    up

--- ch09/src/App.js
+++ ch09/src/App.js
@@ -1,56 +1,5 @@
-import './AppNavLink.css'
-import { Route, Routes, NavLink, Link } from 'react-router-dom'
-
-function Home() {
-   return (
-      <div>
-         <h2>Home</h2>
-         Home...
-      </div>
-   )
-}
-
-function Topics() {
-   return (
-      <div>
-         <h2>Topics</h2>
-         Topics...
-      </div>
-   )
-}
-
-function Contact() {
-   return (
-      <div>
-         <h2>Contact</h2>
-         Contact...
-      </div>
-   )
-}
-
 function App() {
-   return (
-      <div>
-         <h1>리액트 라우터</h1>
-         <ul>
-            <li>
-               <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-               <NavLink to="/topics">Topics</NavLink>
-            </li>
-            <li>
-               <NavLink to="/contact">Contact</NavLink>
-            </li>
-         </ul>
-         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/topics" element={<Topics />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/*" element={'Not Found'} />
-         </Routes>
-      </div>
-   )
+   return <></>
 }
 
 export default App
