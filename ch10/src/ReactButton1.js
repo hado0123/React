0578a7c11dcc2619f725eb3066e07c80e24dfd41
hado0123 <hado_123@naver.com>
2024-11-06 11:59:30 +0900

    up

--- ch10/src/ReactButton1.js
+++ ch10/src/ReactButton1.js
@@ -1,8 +1,10 @@
 function ReactButton1() {
+   // 기존 리액트에서 인라인 스타일 적용하는 방법
    const style = {
       color: 'white',
       backgroundColor: 'purple',
    }
+
    return <button style={style}>버튼</button>
 }
 
