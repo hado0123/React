16b1706020739b9e2cc1814e2371836a8d8d651a
hado0123 <hado_123@naver.com>
2025-06-10 17:06:56 +0900

    up

---
+++ tmdb-final/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Detail from './pages/Detail'
+import MovieCategory from './pages/MovieCategory'
+import SearchResults from './pages/SearchResults'
+import NotFound from './pages/NotFound'
+
+function App() {
+   return (
+      <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/popular" element={<MovieCategory category="popular" />} />
+         <Route path="/now_playing" element={<MovieCategory category="now_playing" />} />
+         <Route path="/upcoming" element={<MovieCategory category="upcoming" />} />
+         <Route path="/search" element={<SearchResults />} />
+         <Route path="/detail/:movieId" element={<Detail />} />
+         <Route path="/*" element={<NotFound />} />
+      </Routes>
+   )
+}
+
+export default App
---
+++ tmdb-final/src/components/Banner.jsx
@@ -0,0 +1,42 @@
+import { Input, Button } from '../styles/StyledComponent'
+import React, { useState, useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './css/Banner.css'
+
+function Banner() {
+   const [searchQuery, setSearchQuery] = useState('')
+   const navigate = useNavigate()
+
+   const handleInputChange = useCallback((event) => {
+      setSearchQuery(event.target.value)
+   }, [])
+
+   const handleSearch = useCallback(
+      (event) => {
+         event.preventDefault() //기본 sumbit 버튼의 성질 방지
+         if (searchQuery.trim()) {
+            //공백 제거 함수
+            // navigate(이동할 경로)
+            navigate(`/search?query=${searchQuery}`) //검색어를 query 파라미터로 전달
+         }
+      },
+      [searchQuery, navigate]
+   )
+
+   return (
+      <div className="banner">
+         <div className="search">
+            <h1 className="header_msg">환영합니다! 수백만 개의 영화를 지금 살펴보세요.</h1>
+
+            <form className="search_form" onSubmit={handleSearch}>
+               <Input $height="40px" $fontSize="1.1rem" value={searchQuery} onChange={handleInputChange} />
+               <Button $width="100px" type="submit">
+                  검색
+               </Button>
+            </form>
+         </div>
+      </div>
+   )
+}
+
+export default Banner
